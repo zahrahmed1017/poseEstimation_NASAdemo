822c287f0ba6b95fce27efe0a1f25e25807ce526
Zahra Ahmed <zahra.ahmed1017@gmail.com>
2024-09-16 09:53:20 -0700

    Adding newer training checkpoint for KRN and updating path in poseEstimationPipelineExample

--- poseEstimationPipelineExample.m
+++ poseEstimationPipelineExample.m
@@ -4,7 +4,7 @@ clear all; close all; clc
 % loop because it only needs to be done once.
 cameraJSON = "cameraNEW.json"; % Path to camera intrinsics json file for plotting wireframe
 chkpt_odn  = "odn_speedplusv2_net_checkpoint__50000__2024_09_14__01_13_49.mat"; % Name of ODN checkpoint, shouldn't need to change this
-chkpt_krn  = "krn_speedplusv2_net_checkpoint__49950__2024_09_15__07_58_04.mat"; % Name of KRN checkpoint, shouldn't need to change this
+chkpt_krn  = "krn_speedplusv2_net_checkpoint__74925__2024_09_16__07_50_03"; % Name of KRN checkpoint, shouldn't need to change this
 
 [odn, krn, sat3dPoints, satEdges, camera] = initializePoseEstimationCNN(cameraJSON, chkpt_odn, chkpt_krn);
 
@@ -13,6 +13,6 @@ chkpt_krn  = "krn_speedplusv2_net_checkpoint__49950__2024_09_15__07_58_04.mat"; 
 
 %%% For testing only, loading in an image, in the demo, this should just be
 %%% the acquired TRON image
-I = imread("TRON_images_NASA/img000137.jpg");
+I = imread("TRON_images_NASA/img000136.jpg");
 
 estimatePose(I, odn, krn, sat3dPoints, satEdges, camera)
